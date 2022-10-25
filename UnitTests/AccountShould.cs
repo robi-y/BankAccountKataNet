41212bb7bab6a4aad7fa6fe3f7d83e2a10c05b39
Reuven Y <robi@post.jce.ac.il>
2022-10-25 12:51:01 +0300

    RED: add first unit test

---
+++ UnitTests/AccountShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace BankAccountKata.UnitTests
+{
+    public class AccountShould
+    {
+        [Fact]
+        public void LogDepositToTransactionRepository()
+        {
+            var transactionManager = Substitute.For<ITransactionManager>();
+            var account = new Account();
+
+            account.Deposit(100);
+
+            transactionManager.Received().LogDeposit(100);
+        }
+    }
+
+    public interface ITransactionManager
+    {
+        void LogDeposit(int amount);
+    }
+}
