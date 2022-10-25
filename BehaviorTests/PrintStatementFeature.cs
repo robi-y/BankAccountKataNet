9f8158a8092fd6d4fe6e981656c82917d2cf1ec8
Reuven Y <robi@post.jce.ac.il>
2022-10-25 12:47:17 +0300

    RED: First behavior test fails

---
+++ BehaviorTests/PrintStatementFeature.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace BankAccountKata.BehaviorTests
+{
+    public class PrintStatementFeature
+    {
+        private Console console = Substitute.For<Console>();
+
+        [Fact]
+        public void PrintAllClientStatements()
+        {
+            var account = new Account();
+
+            account.Deposit(1000);
+            account.Withdraw(100);
+            account.Deposit(500);
+
+            account.PrintStatement();
+
+            Received.InOrder(() =>
+            {
+                console.Received().WriteLine("DATE       | AMOUNT  | BALANCE");
+                console.Received().WriteLine("10/11/2015 |  500.00 | 1400.00");
+                console.Received().WriteLine("02/11/2015 | -100.00 | 900.00");
+                console.Received().WriteLine("01/11/2015 | 1000.00 | 1000.00");
+                
+            });        
+        }
+    }
+}
---
+++ Console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountKata
+{
+    public class Console
+    {
+        public void WriteLine(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
