688f5e50e7ee730f25073181cc2a0e9d65d81e57
Reuven Y <robi@post.jce.ac.il>
2022-10-25 13:27:35 +0300

    RED: unit test for printing empty statements

--- Account.cs
+++ Account.cs
@@ -8,28 +8,28 @@ namespace BankAccountKata
 {
     public class Account
     {
-        private ITransactionRepository _transactionRepository;
+        private ITransactionRepository transactionRepository;
         private StatementPrinter statementPrinter;
 
         public Account(ITransactionRepository _transactionRepository, StatementPrinter statementPrinter)
         {
-            this._transactionRepository = _transactionRepository;
+            this.transactionRepository = _transactionRepository;
             this.statementPrinter = statementPrinter;
         }
 
         public void Deposit(int amount)
         {
-            _transactionRepository.LogDeposit(amount);
+            transactionRepository.LogDeposit(amount);
         }
 
         public void Withdraw(int amount)
         {
-            _transactionRepository.LogWithdrawal(amount);
+            transactionRepository.LogWithdrawal(amount);
         }
 
         public void PrintStatement()
         {
-            var statements = _transactionRepository.AllStatements;
+            var statements = transactionRepository.AllStatements;
             statementPrinter.Print(statements);
         }
 
---
+++ UnitTests/StatementPrinterShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace BankAccountKata.UnitTests
+{
+    public class StatementPrinterShould
+    {
+        private List<Statement> EMPTY_STATEMENT = new List<Statement>();
+
+        [Fact]
+        public void PrintHeaderForEmptyStatementList()
+        {
+            Console console = Substitute.For<Console>();
+            var printer = new StatementPrinter();
+
+            printer.Print(EMPTY_STATEMENT);
+
+            console.Received().WriteLine("DATE       | AMOUNT  | BALANCE");
+        }
+    }
+}
