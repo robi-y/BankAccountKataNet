f39cebf67946c1301b4b2f7c7519625966b738b9
Reuven Y <robi@post.jce.ac.il>
2022-10-25 12:28:17 +0300

    Add account class from Kata constrains

---
+++ Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountKata
+{
+    public class Account
+    {
+        public void Deposit(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(int amount)
+        {
+            throw new NotImplementedException();  
+        }
+
+        public void PrintStatement()
+        {
+            throw new NotImplementedException();    
+        }
+
+    }
+}
